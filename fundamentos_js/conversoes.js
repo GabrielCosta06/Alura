ad2f84e38223157ae422ba2b10d9081ed380ac6e
GabrielCosta06 <josetropilo@gmail.com>
2023-11-18 17:41:14 -0300

    Lições ALURA

---
+++ fundamentos_js/conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleano
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+//conversão explícita
+
+const numeroDois = 567;
+const NumeroStringDois = Number("567");
+console.log(numeroDois + NumeroStringDois);
---
+++ fundamentos_js/type_string.js
@@ -0,0 +1,8 @@
+const texto1 = "olá mundo!";
+const texto2 = 'olá mundo 2!';
+const senha = "dasdasdad";
+
+const citacao = "meu nome é ";
+const seuNome = "Leonardo";
+
+console.log(citacao + seuNome);
