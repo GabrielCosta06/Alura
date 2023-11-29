26cee4d652ff9d0e7b17c8d7be1d590a87e51cf3
José Gabriel de Lima Santos Costa <josetropilo@gmail.com>
2023-11-29 11:52:55 -0300

    Callback.js

---
+++ js_arrays/callbacks.js
@@ -0,0 +1,15 @@
+const nomes = ['Pedro', 'Ivan', 'Roberto'];
+
+// nomes.forEach(function(nome) {
+//     console.log(nome);
+// });
+
+// nomes.forEach((nome) => {
+//     console.log(nome);
+// });
+
+function imprimeNome(nome) {
+    console.log(nome);
+}
+
+nomes.forEach(imprimeNome);
