b71ff65daffe1fc99475f86b938573cc61399d5e
José Gabriel de Lima Santos Costa <josetropilo@gmail.com>
2023-12-02 22:27:13 -0300

    Mais lições completadas

---
+++ objetos_js/acesso_ponto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: 'Andre',
+    idade: 32,
+    cpf: '12312314',
+    email: 'andre@dominio.com'
+};
+
+console.log(`O nome do cliente é: ${cliente.nome}`);
---
+++ objetos_js/objeto.js
@@ -0,0 +1,13 @@
+// const listaCPFs = [131313, 1414141, 151515];
+
+// const informacoesPessoa = ['nome', 'jose', 'idade', 17, 'cpf', '224142'];
+
+// console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: 'José',
+    idade: 17,
+    cpf: '131313'
+}
+
+console.log(objetoPessoa)
