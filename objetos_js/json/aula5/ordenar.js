9ae098b14d3114de87395d78439d68fd73d4c4bb
José Gabriel de Lima Santos Costa <josetropilo@gmail.com>
2023-12-07 12:48:32 -0300

    Json

---
+++ objetos_js/json/aula5/ordenar.js
@@ -0,0 +1,20 @@
+const clientes = require("./clientes.json");
+
+function ordenar(lista, propriedade) {
+  const resultado = lista.sort((a, b) => {
+    if (a[propriedade] < b[propriedade]) {
+      return -1;
+    } else if (a[propriedade] > b[propriedade]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  return resultado;
+}
+
+const ordenadoNome = ordenar(clientes, 'nome');
+const ordenadoInverso = ordenadoNome.reverse();
+
+console.log(ordenadoNome)
